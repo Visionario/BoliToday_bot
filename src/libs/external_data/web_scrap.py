c4b4eaf2e1684546fe49dba8dca2a381ac24f3de
fcoagz <grihardware@gmail.com>
2023-06-28 17:46:50 -0400

    replace the web scraping wit the bolicoin API

--- src/libs/external_data/web_scrap.py
+++ src/libs/external_data/web_scrap.py
@@ -1,36 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
-
+import json
 
 class BolisInfo:
     """
-    Get information from https://bolis.info
+    Get information from https://api.bolivarcoin.tech//help_docs
 
     Some code by Francisco "Cisco" Griman, https://github.com/fcoagz
     """
 
     def __init__(self):
-        _response = 'https://bolis.info/'
-        content = self._response_in_return_content(requests.get(_response))
-        soup = BeautifulSoup(content, features="lxml")
+        _content = self._get_content(requests.get('https://api.bolivarcoin.tech/'))
+        suffix = 'K' if _content['blockchain']['difficulty'] < 999999 else 'M'
+        self.difficulty = f"{round(_content['blockchain']['difficulty'] / 1000, 2):.2f} {suffix}"
+        self.hash_rate = _content['networkinfo']['connections']
+        self.active_masternodes = _content['masternodes']['enabled']
 
-        self.hash_rate = f'{self._get_value_bolis_info(soup)[0][3]} {self._get_value_bolis_info(soup)[0][4]}'
-        self.difficulty = f'{self._get_value_bolis_info(soup)[0][6]} {self._get_value_bolis_info(soup)[0][7]}'
-
-        self.last_block = f'{self._get_value_bolis_info(soup)[1][3]}'
-        self.last_block_update = f'{self._get_value_bolis_info(soup)[1][5]} {self._get_value_bolis_info(soup)[1][6]} {self._get_value_bolis_info(soup)[1][7]}'
-
-        self.active_masternodes = f'{self._get_value_bolis_info(soup)[2][2]}'
-        self.expired_masternodes = f'{self._get_value_bolis_info(soup)[2][4]}'
-
-        # optional replace , by .
-        self.coins_issued = f'{self._get_value_bolis_info(soup)[3][2]}'.replace(',', '.')
-        self.coins_to_issue = f'{self._get_value_bolis_info(soup)[3][5]}'.replace(',', '.')
-
-    def _response_in_return_content(self, response: requests.Response) -> bytes:
+    def _get_content(self, response: requests.Response):
         if not response.status_code == requests.codes.ok:
-            raise ValueError('Cannot connect to bolis.info server')
-        return response.content
-
-    def _get_value_bolis_info(self, soup: BeautifulSoup) -> list:
-        return [list(value.text.split(' ')) for value in soup.find_all('div', 'card-body')]
+            raise ValueError('Cannot connect to api.bolivarcoin.tech server')
+        return json.loads(response.content.decode('utf-8'))
