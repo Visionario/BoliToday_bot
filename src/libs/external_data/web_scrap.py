ef49d13ae7205a457b3916ed05e79a7a97c2e915
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2023-06-28 17:55:16 -0400

    not sure about the hash rate.

--- src/libs/external_data/web_scrap.py
+++ src/libs/external_data/web_scrap.py
@@ -12,7 +12,7 @@ class BolisInfo:
         _content = self._get_content(requests.get('https://api.bolivarcoin.tech/'))
         suffix = 'K' if _content['blockchain']['difficulty'] < 999999 else 'M'
         self.difficulty = f"{round(_content['blockchain']['difficulty'] / 1000, 2):.2f} {suffix}"
-        self.hash_rate = _content['networkinfo']['connections']
+        # self.hash_rate = _content['networkinfo']['connections']
         self.active_masternodes = _content['masternodes']['enabled']
 
     def _get_content(self, response: requests.Response):
